a2f0a1e35de5c769254be8a3e23af5c65971c1e0
wolfc01 <wolfc01@97710ed2-df4c-46bb-bbc7-61a73635fa48>
2012-03-17 20:42:26 +0000

    added minor version

--- make_ubuntu_package.sh
+++ make_ubuntu_package.sh
@@ -1,2 +1,10 @@
-tar -cvzf  ../procexp_1.0.orig.tar.gz ./procexp
+SVNVERSION=`svnversion`
+
+echo "procexp (1.0.$SVNVERSION-0ubuntu1) precise; urgency=low
+
+  * Initial release (Closes: #nnnn)  <nnnn is the bug number of your ITP>
+
+ -- Carl Wolff <carl@wolff-online.nl>  Thu, 15 Mar 2012 22:10:05 +0100" > ./debian/changelog
+
+tar -cvzf  ../procexp_1.0.$SVNVERSION.orig.tar.gz ./procexp
 debuild -us -uc
