9071b4630d2eddc394089745e51498f84c971ba6
wolfc01 <wolfc01@97710ed2-df4c-46bb-bbc7-61a73635fa48>
2012-03-22 14:13:51 +0000

    better packaging

--- make_ubuntu_package.sh
+++ make_ubuntu_package.sh
@@ -8,4 +8,4 @@ echo "procexp (1.0.$SVNVERSION-0ubuntu1) precise; urgency=low
  -- Carl Wolff <carl@wolff-online.nl>  Thu, 15 Mar 2012 22:10:05 +0100" > ./debian/changelog
 
 tar -cvzf  ../procexp_1.0.$SVNVERSION.orig.tar.gz ./procexp
-debuild -us -uc
+debuild -us -uc --source-option=--include-binaries
