f63a02a49798af59f058347bd5e699a645863508
wolfc01 <wolfc01@97710ed2-df4c-46bb-bbc7-61a73635fa48>
2012-11-24 20:55:46 +0000

    Bux fixes release1.2

---
+++ make_ubuntu_package.sh
@@ -0,0 +1,11 @@
+find . -name "*.pyc"|xargs rm -f 
+SVNVERSION=`svnversion`
+
+echo "procexp (1.2.$SVNVERSION-0ubuntu1) precise; urgency=low
+
+  * Release 1.2 (Closes: #nnnn)  <nnnn is the bug number of your ITP>
+
+ -- Carl Wolff <procexp@wolff-online.nl>  Thu, 11 Nov 2012 22:10:05 +0100" > ./debian/changelog
+
+tar -cvzf  ../procexp_1.2.$SVNVERSION.orig.tar.gz ./procexp
+debuild -us -uc --source-option=--include-binaries
