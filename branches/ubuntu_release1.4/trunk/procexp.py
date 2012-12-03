823c516067cdb3191b8bdc133059b229a9bd57e0
wolfc01 <wolfc01@97710ed2-df4c-46bb-bbc7-61a73635fa48>
2012-12-03 11:24:37 +0000

    fix for rel1.4: using sudo for tcpdump for better recurity

--- branches/ubuntu_release1.4/trunk/procexp.py
+++ branches/ubuntu_release1.4/trunk/procexp.py
@@ -422,13 +422,19 @@ def updateUI():
 
     #copy processed to be deleted to the red list      
     for proc in closedProc:
-      redTopLevelItems[proc] = treeProcesses[proc]
+      try:
+        redTopLevelItems[proc] = treeProcesses[proc]
+      except KeyError:
+        pass
      
         
     #color all deleted processed red 
     for proc in redTopLevelItems:
-      for column in xrange(redTopLevelItems[proc].columnCount()):
-        redTopLevelItems[proc].setBackgroundColor(column, QtGui.QColor(255,0,0))
+      try:
+        for column in xrange(redTopLevelItems[proc].columnCount()):
+          redTopLevelItems[proc].setBackgroundColor(column, QtGui.QColor(255,0,0))
+      except RuntimeError:
+        pass 
     
     #update status information about the processes  
     try:
@@ -527,7 +533,6 @@ loadSettings()
 
 timer.start(int(settings["updateTimer"]))  
 
-tcpip_stat.start()
 MainWindow.show()
 
 reader = procreader.reader.procreader(int(settings["updateTimer"]), int(settings["historySampleCount"]))
